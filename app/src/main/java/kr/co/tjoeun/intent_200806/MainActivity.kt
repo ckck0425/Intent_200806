ffa3cf68434d1d29c8cc070a4e3b1dd846de413d
yugyeong ko <ckck0425ko@gmail.com>
2020-08-06 22:25:55 +0900

    일단 완료 누르면 돌아가게만 처리

--- app/src/main/java/kr/co/tjoeun/intent_200806/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200806/MainActivity.kt
@@ -3,9 +3,14 @@ package kr.co.tjoeun.intent_200806
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nic_name.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    1000이라는 숫자가 > 닉네임을 변경하는 요청이라는걸 인식하기 매우 어렵다
+//    별개의 멤버변수로 1000을 담아서 이름을 보고 알수 있도록 처리하자
+    val REQ_FOR_NICKNAME = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,18 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("message",inputMessage)
             startActivity(myIntent)
         }
+
+        changeNickNameBtn.setOnClickListener {
+//        닉네임 변경화면으로 이동 > 새 닉네임을 받으러 이동한다고 명시
+
+            val myItent = Intent(this, EditNicNameActivity::class.java)
+//          단순 이동 X, 결과를 받으러 이동 => 어떤 결과를 받으러 가는건지 명시.
+//            => 고유 숫자를 임의로 지정해서 명시. => 멤버변수로 담아서 가독성 향상
+            startActivityForResult(myItent,1000)
+
+        }
+
+
 
     }
 }
