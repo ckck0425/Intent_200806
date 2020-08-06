ae7cfd2f31850359d35c8c0189d52d28451c993d
yugyeong ko <ckck0425ko@gmail.com>
2020-08-06 22:07:44 +0900

    닉네임변경화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/intent_200806/EditNicNameActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.intent_200806
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nic_name)
+    }
+}
