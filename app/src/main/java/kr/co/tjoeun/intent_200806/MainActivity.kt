9cd66fe2477283f86b860440c6eacf7d22685c96
yugyeong ko <ckck0425ko@gmail.com>
2020-08-10 20:12:31 +0900

    다이얼 액션 예시

--- app/src/main/java/kr/co/tjoeun/intent_200806/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_200806/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.intent_200806
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -17,6 +18,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        dialBtn.setOnClickListener {
+            val inputPhoneNum = phoneNumEdt.text.toString()
+//             어디에 전화를 걸지 Uri를 이용해서 정보 저장
+            val myUri = Uri.parse("tel:${inputPhoneNum}")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+            startActivity(myIntent)
+
+
+        }
+
+
 
         moveToFirstBtn.setOnClickListener {
 //            FirstActivity로 이동하기 => Intent
